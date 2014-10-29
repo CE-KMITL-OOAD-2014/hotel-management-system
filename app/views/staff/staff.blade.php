2bd9228b94b065436b1d0dc8c03798b1e1ffa0f3
lllove38 <nathakit_999@hotmail.com>
2014-10-29 16:26:46 +0700

    print name hotel in view staff

--- app/views/staff/staff.blade.php
+++ app/views/staff/staff.blade.php
@@ -9,6 +9,14 @@
 <h1>This is  my staff</h1>
 <p>This page is created using a master template.</p>
 
+<?php $users=User::find(Auth::id());?>
+@if(Authority::getCurrentUser()->hasRole('manager'))
+    @foreach($users->hotels as $hotel)
+    	<li>{{ $hotel->name  }}
+
+</li>
+    @endforeach
+   @endif
     <!-- Login & Register button -->
     <div class="control-group">
         <div class="controls">

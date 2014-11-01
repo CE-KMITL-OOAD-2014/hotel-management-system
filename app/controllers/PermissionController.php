ac6d28d8f00b5a43ad69dad5d21768f31af58bab
lllove38 <nathakit_999@hotmail.com>
2014-11-01 11:53:19 +0700

    edit postSetPermission

--- app/controllers/PermissionController.php
+++ app/controllers/PermissionController.php
@@ -9,31 +9,29 @@ class PermissionController extends BaseController {
         ->with ('staff_id',user::find($member_id));
 		$member = User::find($member_id);
 		$hotel = Hotel::find($hotel_id);
-		$permission = new Permission;
-        $permission->user_id = $member_id;
-        $permission->view_room = 1;
-        $permission->change_status_room = 1;
-        $permission->view_guest = 1;
-        $permission->create_guest = 1;
-        $permission->save();
-  //return Redirect::to('myhotel/'.$hotel_id)->with('success', 'You have successfully accept '.$member->name.' as staff.');      
+		
+        
 	}
 	public function postSetPermission($hotel_id,$member_id)
-	{
-		return View::make('permission.permission') 
-		->with ('hotel_id',hotel::find($hotel_id))
-        ->with ('staff_id',user::find($member_id));
-		$member = User::find($member_id);
-		$hotel = Hotel::find($hotel_id);
-		$permission = new Permission;
+	 {
+	 	$permission = new Permission;
         $permission->user_id = $member_id;
         $permission->view_room = 1;
         $permission->change_status_room = 1;
         $permission->view_guest = 1;
         $permission->create_guest = 1;
         $permission->save();
-  //return Redirect::to('myhotel/'.$hotel_id)->with('success', 'You have successfully accept '.$member->name.' as staff.');      
-	}
+
+	return Redirect::to('myhotel/'.$hotel_id)->with('success', 'You have successfully set'.$member->name.' permission.');
+            // Redirect to home with success message
+        //    return Redirect::to('myhotel/'.$hotel->id)->with('success', 'You have successfully set permission');
+        
+        //else
+        // Something went wrong.
+        //return Redirect::to('create_room')->withErrors($validator)->withInput(Input::except('fail'));
+        
+
+    }
 
 
 

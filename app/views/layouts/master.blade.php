9ac0adcf9b65e7bcb5d072f0b42a69ebcfdd7072
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-03 16:49:50 +0700

    Fix bootstrap need jquery

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -80,8 +80,8 @@
 
         </div>
     @section('js')
-    {{ HTML::script('js/bootstrap.min.js') }}
     {{ HTML::script('js/jquery-1.11.1.min.js')}}
+    {{ HTML::script('js/bootstrap.min.js') }}
     @show
     </body>
 </html>

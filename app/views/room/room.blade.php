ae2bfaf4418dbb53dd1c05345aefbba7ce1b57ac
lllove38 <nathakit_999@hotmail.com>
2014-11-05 12:35:01 +0700

    success room permission

--- app/views/room/room.blade.php
+++ app/views/room/room.blade.php
@@ -8,29 +8,44 @@
 @section('content')
 <h1>This is my room!</h1>
     <!-- Login & Register button -->
-    <div class="control-group">
-        <div class="controls">
-@if(Authority::getCurrentUser()->hasRole('manager'))
-            {{ HTML::link('create_room/'.$hotel_id, 'Create room') }}
-            @endif
-        </div>
-    </div>
-    <?php 
+<?php 
     $hotels=Hotel::find($hotel_id);
     $user = User::find(Auth::id());
-    ?>
-      @foreach($hotels->rooms as $room)
+?>
+@if(Authority::getCurrentUser()->hasRole('manager'))
+    {{ HTML::link('create_room/'.$hotel_id, 'Create room') }}
+    @foreach($hotels->rooms as $room)
         @foreach($room->statusrooms as $status)
-        <li>{{ $room->roomnumber}}
-        {{ $room->price}}
-        {{ $room->detail}}
-        {{ $status->name}}
+            <li>
+            {{ $room->roomnumber}}
+            {{ $room->price}}
+            {{ $room->detail}}
+            {{ $status->name}}
             {{ HTML::link('edit_room/'.$hotel_id.'/'.$room->id, 'Edit room') }}
             {{ HTML::link('empty','Empty')}}
             {{ HTML::link('occupied', 'Occupied') }}
             {{ HTML::link('reserved', 'Reserved') }}
             {{ HTML::link('maintenance', 'Maintenance') }}
-</li>
+            </li>
+            @endforeach
+        @endforeach 
+@elseif($user->permissions->view_room==1)
+    @foreach($hotels->rooms as $room)
+        @foreach($room->statusrooms as $status)
+            <li>
+            {{ $room->roomnumber}}
+            {{ $room->price}}
+            {{ $room->detail}}
+            {{ $status->name}}            
+            @if($user->permissions->manage_room==1 )
+                {{ HTML::link('edit_room/'.$hotel_id.'/'.$room->id, 'Edit room') }}
+                {{ HTML::link('empty','Empty')}}
+                {{ HTML::link('occupied', 'Occupied') }}
+                {{ HTML::link('reserved', 'Reserved') }}
+                {{ HTML::link('maintenance', 'Maintenance') }}
+                </li>
+                @endif
         @endforeach
     @endforeach
+@endif
 @stop

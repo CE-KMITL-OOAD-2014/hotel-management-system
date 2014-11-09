ebf54a6d3cde0f49a37344974ca5b842d39b87a8
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-10 00:01:19 +0700

    login.blade : CSS cahnge

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -7,39 +7,37 @@
 
 {{-- Content --}}
 @section('content')
-<div class="page-header">
-    <h2>Login into your account</h2>
-</div>
 
+<div class="well col-lg-3 center-block" style="float: none;">
 {{ Form::open(array('url' => 'login', 'class' => 'form-horizontal')) }}
-
+<fieldset>
+    <legend>Login</legend>
     <!-- Name -->
-    <div class="control-group {{{ $errors->has('username') ? 'error' : '' }}}">
-        {{ Form::label('username', 'Username', array('class' => 'control-label')) }}
+    <div class="form-group">
+        {{ Form::label('inputusername', 'Username', array('class' => 'control-label')) }}
 
-        <div class="controls">
-            {{ Form::text('username', Input::old('username')) }}
+            {{ Form::text('username', Input::old('username'),array('class'=>'form-control  input-sm','id'=>'inputusername','style'=>'background-image: none; background-position: 0% 0%; background-repeat: repeat;')) }}
             {{ $errors->first('username') }}
-        </div>
+        
     </div>
 
     <!-- Password -->
-    <div class="control-group {{{ $errors->has('password') ? 'error' : '' }}}">
-        {{ Form::label('password', 'Password', array('class' => 'control-label')) }}
+    <div class="form-group">
+        {{ Form::label('inputpassword', 'Password', array('class' => 'control-label')) }}
 
-        <div class="controls">
-            {{ Form::password('password') }}
+            {{ Form::password('password',array('class'=>'form-control  input-sm','id'=>'inputpassword','style'=>'background-image: none; background-position: 0% 0%; background-repeat: repeat;'))}}
             {{ $errors->first('password') }}
-        </div>
+
     </div>
 
     <!-- Login & Register button -->
     <div class="control-group">
         <div class="controls">
-            {{ Form::submit('Login', array('class' => 'btn')) }}
-            {{ HTML::link('register', 'Register') }}
+            {{ Form::submit('Login', array('class' => 'btn btn-primary')) }}
+            {{ HTML::link('register', 'Register',array('class' => 'btn btn-default')) }}
         </div>
     </div>
-
+</fieldset>
 {{ Form::close() }}
+</div>
 @stop

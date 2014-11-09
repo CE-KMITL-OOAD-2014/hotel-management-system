84e6d265410eda79cff6c4dc0f6f9c7b049eb9ba
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-09 14:46:48 +0700

    home.blade : delete Authority

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -9,17 +9,4 @@
 <h1>Hotel management system</h1>
 <h2>Featrue :</h2>
 
-<?php
-if(Auth::check()){
-	if( Authority::can('manage', 'all') ) {
-		echo 'You are admin id: ';
-		echo Auth::id();
-	}elseif (Authority::can('moderate', 'User')) {
-		echo 'You are member id: ';
-		echo Auth::id();
-	}
-
-}
-
-?>
 @stop

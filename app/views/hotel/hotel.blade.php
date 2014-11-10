6daa6abb172613408034e648e0f8dcc9e3f52069
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-10 15:03:24 +0700

    hotel.blade : CSS change

--- app/views/hotel/hotel.blade.php
+++ app/views/hotel/hotel.blade.php
@@ -11,49 +11,89 @@
 <?php $users=User::find(Auth::id());?>
 <!-- Staff can't create hotel -->
 @if( $users->role != 'staff')
-    {{ HTML::link('create_hotel', 'Create hotel') }}<br><br>
+    {{ HTML::link('create_hotel', 'Create hotel',array('class' => 'btn btn-info')) }}<br><br>
     @endif
-
-
 
 <!--only Manager can Edit his hotels-->
 @if( $users->role == 'manager' )
-
+<?php $hotelNumber = 1;?>
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr class="active">
+            <th>#</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Telephone No.</th>
+            <th></th>
+        </tr>
+    </thead>
     @foreach($users->hotels as $hotel)
-        {{'hotel name : '}}
-        {{ HTML::link('hotel/'.$hotel->id,$hotel->name ) }}<br>
-    	{{'address : '.$hotel->address}}<br>
-    	{{'telephone number : '.$hotel->tel}}<br>
-        {{ HTML::link('edit_hotel/'.$hotel->id,'edit '.$hotel->name ) }} <br> <br> 
-   
+    <tbody>
+        <tr>
+            <td>{{ $hotelNumber }}</td>
+            <td>{{ HTML::link('hotel/'.$hotel->id,$hotel->name ) }}</td>
+            <td>{{ $hotel->address }}</td>
+            <td>{{ $hotel->tel }}</td>
+            <td>{{ HTML::link('edit_hotel/'.$hotel->id, 'Edit',array('class' => 'btn btn-default btn-sm')) }}</td>
+        </tr>
+        <?php $hotelNumber++;?>
     @endforeach
+    </tbody>
+</table>
 <!-- Staff can see detail hotel -->
 @elseif( $users->role == 'staff' )
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr class="active">
+            <th>Name</th>
+            <th>Address</th>
+            <th>Telephone No.</th>
+            <th></th>
+        </tr>
+    </thead>
     @foreach($users->hotels as $hotel)
-   
-        <!-- check permission staff can view room-->
-        @if($users->permissions->view_room==1&&$users->permissions->manage_room==1)
+
+        <tbody>
+        <tr>
             <!--show url to room-->
-            {{'hotel name : '}}
-            {{ HTML::link('hotel/'.$hotel->id, $hotel->name  ) }}<br>
+                    <!-- check permission staff can view room-->
+        @if($users->permissions->view_room==1&&$users->permissions->manage_room==1)
+            <td>{{ HTML::link('hotel/'.$hotel->id,$hotel->name) }}</td>
         @else 
-            <!--show text-->
-            {{'hotel name : '.$hotel->name }}<br>
+                    <!--show text-->
+            <td>{{ $hotel->name }}</td>
         @endif
-            {{'address : '.$hotel->address}}<br>
-            {{'telephone number : '.$hotel->tel}}<br><br>
-    
+            <td>{{ $hotel->address }}</td>
+            <td>{{ $hotel->tel }}</td>
+        </tr>
     @endforeach
 <!-- Member can sent request for apply to staff of hotel -->
 @else
+<?php $hotelNumber = 1;?>
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr class="active">
+            <th>#</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Telephone No.</th>
+            <th></th>
+        </tr>
+    </thead>
 @foreach($hotels as $hotel)
     
-        {{'hotel name : '.$hotel->name}}<br>
-        {{'address : '.$hotel->address}}<br>
-        {{'telephone number : '.$hotel->tel}}<br>
-        {{HTML::link('join_hotel/'.$hotel->id, ' Join '.$hotel->name) }}<br><br>
-
+        <tbody>
+        <tr>
+            <td>{{ $hotelNumber }}</td>
+            <td>{{ $hotel->name }}</td>
+            <td>{{ $hotel->address }}</td>
+            <td>{{ $hotel->tel }}</td>
+            <td>{{HTML::link('join_hotel/'.$hotel->id, ' Join ',array('class' => 'btn btn-default btn-sm')) }}</td>
+        </tr>
+   <?php $hotelNumber++;?>       
     @endforeach
+      </tbody>
+</table>
 @endif
 
 @stop

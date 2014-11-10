c272588e4b71df4972c6882aebcc752cb5d98cd5
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-10 18:18:07 +0700

    room.blade : CSS change

--- app/views/room/room.blade.php
+++ app/views/room/room.blade.php
@@ -11,34 +11,64 @@
 
 <?php 
 $user=User::find(auth::id());
-      ?>
-<!-- manager can create all rooms and all his hotels -->
+?>
+<!-- Manager can create and edit rooms -->
 @if( $user->role == 'manager' )
-    @foreach($user->hotels as $hotel)
-        <h3>{{'hotel name : '.$hotel->name }}</h3>
-        {{ HTML::link('create_room/'.$hotel->id, 'Create room') }}
-        {{ HTML::link('change_room_status/'.$hotel->id, 'Change room status') }}
-        @foreach($hotel->rooms as $room)
-            <li>
-                {{ $room->roomnumber}}
-                {{ $room->price}}
-                {{ $room->detail}}
-                {{ HTML::link('edit_room/'.$hotel->id.'/'.$room->id, 'Edit room') }}
-            </li>
-
+@foreach($user->hotels as $hotel)
+<h3>{{'Hotel : '.$hotel->name }}</h3>
+{{ HTML::link('create_room/'.$hotel->id, 'Create room',array('class'=>'btn btn-info')) }}
+{{ HTML::link('change_room_status/'.$hotel->id, 'Change room status',array('class'=>'btn btn-warning')) }}
+<?php $roomNumber = 1;?>
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr class="active">
+            <th>#</th>
+            <th>Room Number</th>
+            <th>Price</th>
+            <th>Detail</th>
+            <th></th>
+        </tr>
+    </thead>
+    @foreach($hotel->rooms as $room)
+    <tbody>
+        <tr>
+            <td>{{ $roomNumber }}</td>
+            <td>{{ $room->roomnumber}}</td>
+            <td>{{ $room->price}}</td>
+            <td>{{ $room->detail}}</td>
+            <td>{{ HTML::link('edit_room/'.$hotel->id.'/'.$room->id, 'Edit',array('class' => 'btn btn-default btn-sm')) }}</td>
+        </tr>
+        <?php $roomNumber++;?>
         @endforeach 
-    @endforeach
+    </tbody>
+</table>
+@endforeach
+
 <!-- staff with permission can see rooms in his hotel -->
 @elseif( $user->permissions->view_room == 1)
-    @foreach($user->hotels as $hotel)
-        @foreach($hotel->rooms as $room)
-            <li>
-                {{ $room->roomnumber}}
-                {{ $room->price}}
-                {{ $room->detail}}
-            </li>
- 
+@foreach($user->hotels as $hotel)
+<?php $roomNumber = 1;?>
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr class="active">
+            <th>#</th>
+            <th>Room Number</th>
+            <th>Price</th>
+            <th>Detail</th>
+        </tr>
+    </thead>
+    @foreach($hotel->rooms as $room)
+    <tbody>
+        <tr>
+            <td>{{ $roomNumber }}</td>
+            <td>{{ $room->roomnumber}}</td>
+            <td>{{ $room->price}}</td>
+            <td>{{ $room->detail}}</td>
+        </tr>
+        <?php $roomNumber++;?>
         @endforeach
-    @endforeach
+    </tbody>
+</table>
+@endforeach
 @endif
 @stop

6fc8ffd47e2f92f32fa4de40d044ac86d529520d
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-10 16:55:54 +0700

    request.blade : CSS change :spades:

--- app/views/request/request.blade.php
+++ app/views/request/request.blade.php
@@ -13,22 +13,35 @@
 <?php $user=User::find(Auth::id());?>
 
     @foreach($user->Hotels as $hotel)
-        <!-- show all his hotels -->
+        <!-- show all hotels that belong to this manager-->
     	<h3>{{ $hotel->name  }}</h3>
+<?php $requestNumber = 1;?>
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr class="active">
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>History</th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
     	@foreach($hotel->requestUsers as $member)
-    	
-            <!-- show member require for each hotel -->
-            
-         
-            {{"Name : ".$member->name}}   
-           <br>
-            {{"Lastname : ".$member->lastname}}<br>
-            {{"email : ".$member->email}}<br>
-            {{"work history : ".$member->workhistory}}<br>
-             {{ HTML::link('accept/'.$hotel->id.'/'.$member->id, 'accept') }}
-            {{ HTML::link('decline/'.$hotel->id.'/'.$member->id, 'decline') }}<br><br>
-
+            <!-- show member request for each hotel -->
+             <tbody>
+        <tr>
+            <td>{{ $requestNumber }}</td>
+            <td>{{$member->name.' '.$member->lastname}}</td>
+            <td>{{ $member->email }}</td>
+            <td>{{ $member->workhistory }}</td>
+            <td>{{ HTML::link('accept/'.$hotel->id.'/'.$member->id, 'Accept',array('class' => 'btn btn-default btn-sm')) }}</td>
+            <td>{{ HTML::link('decline/'.$hotel->id.'/'.$member->id, 'Decline',array('class' => 'btn btn-default btn-sm')) }}</td>
+        </tr>
+        <?php $requestNumber++;?>
     	@endforeach
+    </tbody>
+</table>
     @endforeach
 
 @stop

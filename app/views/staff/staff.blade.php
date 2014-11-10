4008462c6ddaa0588421bb006669b78e108778de
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-10 18:41:31 +0700

    staff.blade : CSS change

--- app/views/staff/staff.blade.php
+++ app/views/staff/staff.blade.php
@@ -6,43 +6,74 @@
 @stop
 
 @section('content')
-<h1>This is  my staff</h1>
+<h1>Staff list</h1>
 
 <?php $user=User::find(Auth::id());?>
-<!-- only manager  see edit permission staff -->
+<!-- only manager can see edit permission button -->
 @if($user->role == 'manager')
     <!--show all his hotels -->
     @foreach($user->hotels as $hotel)
     	<h3>{{ $hotel->name  }}</h3>
-    	@foreach($hotel->users as $user_id)
-            <!--show  manger  -->
-            @if($user_id->role == 'manager')
-    	       {{"Manager name : ". $user_id->name." ".$user_id->lastname }}<br>
-                <b>Staff</b><br>
+        <?php $staffNumber = 1;?>
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr class="active">
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th></th>
+        </tr>
+    </thead>
+    	@foreach($hotel->users as $user)
             <!--show  staff  -->
-            @elseif($user_id->role == 'staff')
-                {{ $user_id->name ." ".$user_id->lastname." "}}
-                {{ HTML::link('edit_permission/'.$hotel->id.'/'.$user_id->id, 'Edit permissions') }}
-                <br>
-            @endif      
+            @if($user->role == 'staff')
+            <tbody>
+        <tr>
+            <td>{{ $staffNumber }}</td>
+            <td>{{ $user->name ." ".$user->lastname}}</td>
+            <td>{{ $user->email}}</td>
+            <td>{{ HTML::link('edit_permission/'.$hotel->id.'/'.$user->id, 'Edit',array('class' => 'btn btn-default btn-sm'))}}</td>
+        </tr>
+        <?php $staffNumber++;?>
+            @endif
         @endforeach
+         </tbody>
+</table>
     @endforeach
- <!-- staff can see his friend staffs in his hotel -->
+ <!-- staff can see other staffs in hotel -->
 @elseif($user->role == 'staff')
          <!--show  his hotel -->
         @foreach($user->hotels as $hotel)
             <h3>{{ $hotel->name  }}</h3>
-            @foreach($hotel->users as $user_id)
+             <?php $staffNumber = 1;?>
+<table class="table table-striped table-hover ">
+    <thead>
+        <tr class="active">
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th></th>
+        </tr>
+    </thead>
+            @foreach($hotel->users as $user)
                 <!--show  manger-->
-                @if($user_id->role == 'manager')
-                {{"Manager name : ". $user_id->name." ".$user_id->lastname }}<br>
+                @if($user->role == 'manager')
+                <p><strong>Manager name : </strong>{{$user->name." ".$user->lastname }}</p>
                 <!--show  staff -->
                 <b>Staff</b><br>
-                @elseif($user_id->role == 'staff')
-                    {{ $user_id->name ." ".$user_id->lastname." "}}
-                <br>
-                @endif      
-            @endforeach
+                @elseif($user->role == 'staff')
+                           <tbody>
+        <tr>
+            <td>{{ $staffNumber }}</td>
+            <td>{{ $user->name ." ".$user->lastname}}</td>
+            <td>{{ $user->email}}</td>
+            <td>{{ HTML::link('edit_permission/'.$hotel->id.'/'.$user->id, 'Edit',array('class' => 'btn btn-default btn-sm'))}}</td>
+        </tr>
+        <?php $staffNumber++;?>
+            @endif
+        @endforeach
+         </tbody>
+</table>
         @endforeach
 @endif
 

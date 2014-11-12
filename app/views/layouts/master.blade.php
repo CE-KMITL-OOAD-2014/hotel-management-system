25493e8aa98ee6bf169fd1a92096f823d3236e9c
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-12 12:41:45 +0700

    View : disable zoom on mobile

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -9,7 +9,8 @@
         Hotel management system
         @show
     </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="HandheldFriendly" content="true" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <!-- CSS are placed here -->

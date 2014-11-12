084da6c2868185d4fb228b56094fa94e3f4ab9cf
Nattanon Rungprasert <nara.fiery@gmail.com>
2014-11-12 14:11:31 +0700

    change_room_status : Fix typo

--- app/views/room/change_room_status.blade.php
+++ app/views/room/change_room_status.blade.php
@@ -40,7 +40,7 @@
 
 
 <div class="form-group">
- <label for="date" class="col-lg-3 control-label">Start date</label>
+ <label for="date" class="col-lg-3 control-label">End date</label>
  <div class="col-lg-6">
   {{Form::text('end_date','',array('class'=>'form-control','id'=>'date2','placeholder'=>'Choose end date',
   'autocomplete'=>'off','style' => 'cursor: pointer; background-attachmentund-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QsPDhss3LcOZQAAAU5JREFUOMvdkzFLA0EQhd/bO7iIYmklaCUopLAQA6KNaawt9BeIgnUwLHPJRchfEBR7CyGWgiDY2SlIQBT/gDaCoGDudiy8SLwkBiwz1c7y+GZ25i0wnFEqlSZFZKGdi8iiiOR7aU32QkR2c7ncPcljAARAkgckb8IwrGf1fg/oJ8lRAHkR2VDVmOQ8AKjqY1bMHgCGYXhFchnAg6omJGcBXEZRtNoXYK2dMsaMt1qtD9/3p40x5yS9tHICYF1Vn0mOxXH8Uq/Xb389wff9PQDbQRB0t/QNOiPZ1h4B2MoO0fxnYz8dOOcOVbWhqq8kJzzPa3RAXZIkawCenHMjJN/+GiIqlcoFgKKq3pEMAMwAuCa5VK1W3SAfbAIopum+cy5KzwXn3M5AI6XVYlVt1mq1U8/zTlS1CeC9j2+6o1wuz1lrVzpWXLDWTg3pz/0CQnd2Jos49xUAAAAASUVORK5CYII=); background-attachment: scroll; background-position: 100% 50%; background-repeat: no-repeat;'))}}
